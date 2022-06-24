2a05bae6a72a02bb72f9b8ba6234be8e72698d8e
John Gamboa <gamboa.john@protonmail.com>
2022-06-24 13:04:29 +0800

    updated api js

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 const loadData = async (loc = 'Cabanatuan') => {
     const key = 'eeec839a4d9973095fd520b438167720';
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${loc}&APPID=${key}&units=metric`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${loc}&APPID=${key}&units=metric`;
     const response = await fetch(url, { mode: 'cors' });
 
     if (response.status === 200) {
